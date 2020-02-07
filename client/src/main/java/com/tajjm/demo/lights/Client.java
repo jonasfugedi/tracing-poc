d863174545504e486a2d8b9d3ce8d25d25f67e09
Jonas Fügedi <jonas@fugedi.se>
2020-02-07 17:48:32 +0100

    Forward to logstash/fluentd and launch services using docker compose

--- client/src/main/java/com/tajjm/demo/lights/Client.java
+++ client/src/main/java/com/tajjm/demo/lights/Client.java
@@ -5,6 +5,7 @@ import com.google.protobuf.Timestamp;
 import com.tajjm.demo.lights.proto.LightProxyServiceGrpc;
 import com.tajjm.demo.lights.proto.PongResponse;
 import io.grpc.ManagedChannelBuilder;
+import io.opencensus.common.Scope;
 import io.opencensus.trace.Span;
 import io.opencensus.trace.Tracing;
 import org.slf4j.Logger;
@@ -28,22 +29,25 @@ public class Client {
 
         // Create a scoped span, a scoped span will automatically end when closed.
         // It implements AutoClosable, so it'll be closed when the try block ends.
-        Span span = Tracing.getTracer().spanBuilder("main").startSpan();
+        var tracer = Tracing.getTracer();
+        Span span = tracer.spanBuilder("main").startSpan();
         try {
+            MDC.put("trace", span.getContext().getTraceId().toLowerBase16());
+            MDC.put("spanId", span.getContext().getSpanId().toLowerBase16());
             logger.info("Calling service");
             for (int i = 0; i < 10; i++) {
-                Span childSpan = Tracing.getTracer().spanBuilderWithExplicitParent("call", span).startSpan();
-                try {
+                try (Scope ignore = tracer.spanBuilderWithExplicitParent("LightServiceClient", span).startScopedSpan()) {
+                    Span childSpan = tracer.getCurrentSpan();
                     MDC.put("trace", childSpan.getContext().getTraceId().toLowerBase16());
                     MDC.put("spanId", childSpan.getContext().getSpanId().toLowerBase16());
+                    logger.info("Calling service-b");
                     PongResponse response = blockingStub.ping(Empty.newBuilder().build());
                     Timestamp ts = response.getTimestamp();
                     Instant instant = Instant.ofEpochSecond(ts.getSeconds(), ts.getNanos());
                     LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
                     logger.info("Response message={} datetime={}", response.getMessage(), dateTime);
-                    MDC.clear();
                 } finally {
-                    childSpan.end();
+                    MDC.clear();
                 }
             }
         } finally {
